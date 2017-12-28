d4cf6436ff4e601502d0e89cadef3b4af30a405f
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-28 17:35:40 +0000

    Map being created on screen

--- master.js
+++ master.js
@@ -11,14 +11,14 @@ const level1 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0],
                 [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
                 [1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
+                [1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1],
                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
                 ]
 
@@ -35,27 +35,35 @@ function makeLevel(lev){
             const levObj = {};
             if(lev[i][j] == 0){
                 levObj.type = 'sky';
-                levObj.xLeft = j * tileSize;
-                levObj.xRight = ((j+1) * tileSize)-1;
-                levObj.yTop = i * tileSize;
-                levObj.yBottom = ((i+1) * tileSize)-1;
-                /*
-                const skyTile = document.createElement('div');
-                skyTile.classList += 'skyTile';
-                app.appendChild(skyTile);
-                */
             }
             else if(lev[i][j] == 1){
                 levObj.type = 'grass';
             }
+            levObj.xLeft = j * tileSize;
+            levObj.xRight = ((j+1) * tileSize)-1;
+            levObj.yTop = i * tileSize;
+            levObj.yBottom = ((i+1) * tileSize)-1;
             currLevLog.push(levObj);
         }
-        
     }
+}
+
+function makeLevelDis(){
+   for(let i = 0; i < currLevLog.length; i++){
+       const newTile = document.createElement('div');
+       if(currLevLog[i].type == 'sky'){
+        newTile.classList += 'skyTile';
+       }
+       else if(currLevLog[i].type == 'grass'){
+           newTile.classList += 'grassTile';
+       }
+       app.appendChild(newTile);
+   }
 }
 
 
 (function init(){
     makeLevel(level1);
+    makeLevelDis();
     console.log(currLevLog)
 })()
